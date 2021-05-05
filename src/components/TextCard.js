7a81482726e3b5255be6cde480e52842ecf4cfc9
Peter Phan <peterphan0422@gmail.com>
2021-05-04 22:51:39 -0500

    feat: desktop version styled

--- src/components/TextCard.js
+++ src/components/TextCard.js
@@ -9,17 +9,19 @@ const TextCard = () => {
 				<p className="card-description">Monitors activity to identify project roadblocks</p>
 				<div className="img">{magnifyingGlass}</div>
 			</div>
-			<div className="card team-builder">
-				<h2 className="card-title">Team Builder</h2>
-				<p className="card-description">
-					Scans our talent network to create the optimal team for your project
-				</p>
-				<div className="img">{house}</div>
-			</div>
-			<div className="card karma">
-				<h2 className="card-title">Karma</h2>
-				<p className="card-description">Regularly evaluates our talent to ensure quality</p>
-				<div className="img">{lightBulb}</div>
+			<div className="card-center">
+				<div className="card team-builder">
+					<h2 className="card-title">Team Builder</h2>
+					<p className="card-description">
+						Scans our talent network to create the optimal team for your project
+					</p>
+					<div className="img">{house}</div>
+				</div>
+				<div className="card karma">
+					<h2 className="card-title">Karma</h2>
+					<p className="card-description">Regularly evaluates our talent to ensure quality</p>
+					<div className="img">{lightBulb}</div>
+				</div>
 			</div>
 			<div className="card calculator">
 				<h2 className="card-title">Calculator</h2>
